0440d4e71f369caf3e56949f325fe1429f7364c7
AlexGolubev <golubevaleksej7@gmail.com>
2021-02-04 23:24:23 +0300

    Update City. Add java convension

--- src/main/java/com/golubev/telegrambot/webservice/entity/City.java
+++ src/main/java/com/golubev/telegrambot/webservice/entity/City.java
@@ -1,6 +1,12 @@
 package com.golubev.telegrambot.webservice.entity;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import java.util.List;
 
 @Entity
