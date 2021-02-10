89697b2aa8dcb72bc1ddbd9453e492a53c658ae1
AlexGolubev <golubevaleksej7@gmail.com>
2021-02-10 20:39:56 +0300

    Create CityInfoController

---
+++ src/main/java/com/golubev/telegrambot/webservice/controller/CityInfoController.java
@@ -0,0 +1,25 @@
+package com.golubev.telegrambot.webservice.controller;
+
+import com.golubev.telegrambot.webservice.service.CityInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/city")
+public class CityInfoController {
+
+    private final CityInfoService cityInfoService;
+
+    public CityInfoController(CityInfoService cityInfoService) {
+        this.cityInfoService = cityInfoService;
+    }
+
+    @DeleteMapping("/{cityId}/info/{infoId}")
+    public void deleteCityInfo(@PathVariable("cityId") Integer cityId,
+                               @PathVariable("infoId") Integer infoId){
+        cityInfoService.delete(cityId, infoId);
+    }
+}
