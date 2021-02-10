6a33fa247ba13aa9a7673e8607a67c33429c69b3
AlexGolubev <golubevaleksej7@gmail.com>
2021-02-10 20:38:44 +0300

    Update CityInfoRepository

--- src/main/java/com/golubev/telegrambot/webservice/repository/CityInfoRepository.java
+++ src/main/java/com/golubev/telegrambot/webservice/repository/CityInfoRepository.java
@@ -3,8 +3,12 @@ package com.golubev.telegrambot.webservice.repository;
 import com.golubev.telegrambot.webservice.entity.CityInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface CityInfoRepository extends JpaRepository<CityInfo, Integer> {
+
+    @Transactional
+    void deleteByIdAndCity_id(Integer infoId, Integer cityId);
 
 }
