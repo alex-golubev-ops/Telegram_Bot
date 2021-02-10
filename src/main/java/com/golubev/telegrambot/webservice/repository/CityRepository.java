3f1d746f1a1808bfa42e3cc60e664f6c397701a6
AlexGolubev <golubevaleksej7@gmail.com>
2021-02-10 23:01:13 +0300

    Update CityRepository

--- src/main/java/com/golubev/telegrambot/webservice/repository/CityRepository.java
+++ src/main/java/com/golubev/telegrambot/webservice/repository/CityRepository.java
@@ -4,6 +4,10 @@ import com.golubev.telegrambot.webservice.entity.City;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CityRepository extends JpaRepository<City,Integer> {
+
+    Optional<City> findByName(String name);
 }
